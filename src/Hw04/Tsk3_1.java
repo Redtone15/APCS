74a1dde277dd3175978a58927b2bccc01974b6c0
Redrone15 <ruihang0523@gmail.com>
2025-07-28 12:42:40 +0800

    Lesson04_Complete

--- src/Hw04/Tsk3_1.java
+++ src/Hw04/Tsk3_1.java
@@ -2,16 +2,16 @@ package Hw04;
 
 public class Tsk3_1 {
 
-
-    public static void main(String[] args) {
-        plotSine();
-    }
-
-    public static void plotSine() {
-        for (double x = 0; x <= 10.6; x += 0.2) {
-            int pos = (int) ((Math.sin(x) + 1) * 34.5);
-            System.out.printf("%.1f |%s*%s%n", x, " ".repeat(pos), " ".repeat(69 - pos));
+        public static void main(String[] args) {
+            plotSine();
         }
-    }
+
+        public static void plotSine() {
+            for (double x = 0; x <= 6.2; x += 0.2) {
+                int pos = (int) ((Math.sin(x) + 1) * 34.5);
+                System.out.printf("%.1f |%s*%s%n", x, " ".repeat(pos), " ".repeat(69 - pos));
+            }
+        }
+
 
 }
